58517ca5bafe3ebb54a71cf88c1c167524159022
nguyenntt97 <nguyenng178@gmail.com>
2020-01-21 20:03:47 +0700

    clean code

--- main-server/snk-n2n/snk_api/src/main/scala/com/sonako/snk_api/model/Project.scala
+++ main-server/snk-n2n/snk_api/src/main/scala/com/sonako/snk_api/model/Project.scala
@@ -5,7 +5,6 @@ import scalikejdbc._
 
 case class Project(id: Long, name: String, thumbnail: String)
 object Project extends SQLSyntaxSupport[Project] {
-  override val connectionPoolName = 'my_pool
   override def tableName: String = "projects"
   def apply(rs: WrappedResultSet): Project = Project(
     rs.long("project_id"),
